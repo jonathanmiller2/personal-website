6bc729e9aa0184d583a2588df5c25f7b30b2b193
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-07-31 10:35:47 -0500

    Learning react with tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -2,31 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
+class Square extends React.Component 
+{
+  render() 
+  {
+    return 
+    (
       <button className="square">
-        {/* TODO */}
+        {this.props.value}
       </button>
     );
   }
 }
 
-class Board extends React.Component {
+class Board extends React.Component
+{
+	renderSquare(i)
+	{
+		return <Square value={i} />
+	}
 }
 
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+class Game extends React.Component 
+{
+  render() 
+  {
+    return 
+    (
+      	<div className="game">
+        	<div className="game-board">
+          	<Board />
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+        	<div className="game-info">
+          		<div>{/* status */}</div>
+          		<ol>{/* TODO */}</ol>
+        	</div>
+      	</div>
     );
   }
 }
