3329079e2c776bb66a6caa67494a8e49481a9aad
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-07-31 11:45:40 -0500

    Started over from scratch because i entered error hell

--- src/index.js
+++ src/index.js
@@ -1,50 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class Square extends React.Component 
-{
-  render() 
-  {
-    return 
-    (
-      <button className="square">
-        {this.props.value}
-      </button>
-    );
-  }
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-class Board extends React.Component
-{
-	renderSquare(i)
-	{
-		return <Square value={i} />
-	}
-}
-
-class Game extends React.Component 
-{
-  render() 
-  {
-    return 
-    (
-      	<div className="game">
-        	<div className="game-board">
-          	<Board />
-        </div>
-        	<div className="game-info">
-          		<div>{/* status */}</div>
-          		<ol>{/* TODO */}</ol>
-        	</div>
-      	</div>
-    );
-  }
-}
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
