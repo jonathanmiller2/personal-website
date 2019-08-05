8a6ed32adcf383010e7020ff1fc071d0526abd8a
jonathanmiller2 <jonathanmiller2@hotmail.com>
2019-08-05 00:41:55 -0500

    Experimenting with position: sticky in CSS

--- src/index.js
+++ src/index.js
@@ -2,100 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
-
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-/*
-class ScrollerButton() extends React.Component {
-	handleClick() = () => {
-
-	}
-}
-*/
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if(!squares[i] && !calculateWinner(squares))
-    {
-		squares[i] = this.state.xIsNext	? 'X' : 'O';
-    	this.setState(
-    	{
-    		squares: squares,
-    		xIsNext: !this.state.xIsNext,
-    	});
-    }
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
-  }
-
-  render() 
-  	{
-    	const winner = calculateWinner(this.state.squares);
-    	let status;
-    	if(winner)
-    	{
-    		status = 'Winner:' + winner;
-    	}
-    	else
-    	{
-    		status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
-    	}
-
-    	return (
-      		<div>
-        		<div className="status">{status}</div>
-        		<div className="board-row">
-          				{this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
-        		</div>
-        		<div className="board-row">
-          				{this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
-        		</div>
-        		<div className="board-row">
-          				{this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
-        		</div>
-      		</div>
-    );
-  }
-}
 
 class Base extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="name-display">
+    <div className = "main">
+    	<div className = "nav-bar">
+    		<ul>
+    			<li>
+    				<a href="#/profile" className="button">Jump to profile</a>
+    			</li>
+    		</ul>
+    	</div>
+        <div className="spacing">
           <div>Jonathan Miller</div>
           <div>AA</div>
-          <a href="#C4">Jump to Chapter 4</a>
-          <div>AA</div>
-          <div>AA</div>
-          <div>AA</div>
-          <div>AA</div>
-          <div>AA</div>
-          <div>BB</div>
-          <div id="C4">CC</div>
+          <div>Spacing</div>
+          <div>Spacing</div>
+          <div>Spacing</div>
+          <div>Spacing</div>
+          <div>Spacing</div>
+          <div>Spacing</div>
+          <div id="/profile">CC</div>
           <div>AA</div>
           <div>AA</div>
           <div>AA</div>
@@ -105,25 +33,6 @@ class Base extends React.Component {
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 // ========================================
 
